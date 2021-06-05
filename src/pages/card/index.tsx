c3f7fd1415da4c8cd16e6692ef894cd2430117c7
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-06-05 12:39:51 +0800

    Fix `card` layout

--- src/pages/card/index.tsx
+++ src/pages/card/index.tsx
@@ -5,28 +5,30 @@ export const title = 'Card';
 export const code = `
 export default function Card() {
   return (
-    <div className="m-8 w-1/2 h-auto shadow-lg flex items-start justify-start flex-col border rounded-lg">
-      <div className="w-full flex items-center justify-center border-b">
-        <img
-          src="https://source.unsplash.com/1200x630/?forest"
-          width="1200"
-          height="630"
-          className="w-full h-auto"
-        />
-      </div>
-      <div className="p-4 w-full border-b">
-        <span className="text-xl">Lorem Ipsum</span>
-        <p className="py-1 text-sm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed est viverra ex tincidunt vehicula. Donec pellentesque diam sit amet mi ullamcorper, ac finibus lorem scelerisque.
-        </p>
-      </div>
-      <div className="p-4 w-full flex items-center justify-start flex-row-reverse">
-        <button
-          type="button"
-          className="text-purple-500 hover:text-purple-400"
-        >
-          Action
-        </button>
+    <div className="m-8">
+      <div className="w-1/2 h-auto overflow-hidden shadow-lg flex items-start justify-start flex-col border dark:border-gray-800 rounded-lg">
+        <div className="w-full flex items-center justify-center border-b dark:border-gray-800">
+          <img
+            src="https://source.unsplash.com/1200x630/?forest"
+            width="1200"
+            height="630"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="p-4 w-full border-b dark:border-gray-800">
+          <span className="text-xl">Lorem Ipsum</span>
+          <p className="py-1 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed est viverra ex tincidunt vehicula. Donec pellentesque diam sit amet mi ullamcorper, ac finibus lorem scelerisque.
+          </p>
+        </div>
+        <div className="p-4 w-full flex items-center justify-start flex-row-reverse">
+          <button
+            type="button"
+            className="text-purple-500 hover:text-purple-400"
+          >
+            Action
+          </button>
+        </div>
       </div>
     </div>
   );
