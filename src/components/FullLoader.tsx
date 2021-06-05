eb194a52fc216db9303862c1de0aabd338aa4bb8
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-06-05 13:15:05 +0800

    Fix `FullLoader` to grow if inside flex

--- src/components/FullLoader.tsx
+++ src/components/FullLoader.tsx
@@ -3,7 +3,7 @@ import Spinner from './Spinner';
 
 export default function FullLoader(): JSX.Element {
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="flex-1 w-full h-full flex items-center justify-center">
       <div className="text-blue-500 w-8 h-8">
         <Spinner />
       </div>
