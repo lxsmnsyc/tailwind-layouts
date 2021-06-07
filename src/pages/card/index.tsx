d7a526831786f09a64a3e520b58d245585adfab0
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-06-07 09:57:33 +0800

    Fix `card` layout

--- src/pages/card/index.tsx
+++ src/pages/card/index.tsx
@@ -5,17 +5,23 @@ import React from 'https://cdn.skypack.dev/react';
 import ReactDOM from 'https://cdn.skypack.dev/react-dom';
 
 function Card() {
+  const [loading, setLoading] = React.useState(true);
+
   return (
     <div className="p-8 w-full h-full flex items-center justify-center">
       <div className="w-1/2 h-auto overflow-hidden shadow-lg flex items-start justify-start flex-col border dark:border-gray-800 rounded-lg">
-        <div className="w-full flex items-center justify-center border-b dark:border-gray-800">
+        <div className="w-full flex items-center justify-center border-b dark:border-gray-800 relative">
           <img
             alt="Forest"
             src="https://source.unsplash.com/1200x630/?forest"
             width="1200"
             height="630"
-            className="w-full h-auto"
+            className={\`w-full h-auto transition-opacity duration-300 \${loading ? 'opacity-0' : 'opacity-100'} \`}
+            onLoad={() => {
+              setLoading(false);
+            }}
           />
+          {loading && <div className="absolute w-full h-full top-0 left-0 animate-pulse bg-gray-100 dark:bg-gray-900" />}
         </div>
         <div className="p-4 w-full border-b dark:border-gray-800">
           <span className="text-xl">Lorem Ipsum</span>
@@ -26,7 +32,7 @@ function Card() {
         <div className="p-4 w-full flex items-center justify-start flex-row-reverse">
           <button
             type="button"
-            className="text-purple-500 hover:text-purple-400"
+            className="bg-gray-800 py-2 px-4 text-white rounded-lg hover:bg-gray-700"
           >
             Action
           </button>
@@ -47,22 +53,35 @@ export default function renderApp(root) {
 
 export const VUE_3 = `
 /** @jsx h */
-import { h, createApp, defineComponent } from 'https://cdn.skypack.dev/vue@3.0.11/dist/vue.esm-browser.js';
+import {
+  h,
+  createApp,
+  defineComponent,
+  reactive,
+} from 'https://cdn.skypack.dev/vue@3.0.11/dist/vue.esm-browser.js';
 
 const Card = defineComponent({
   name: 'Card',
   setup () {
+    const state = reactive({
+      loading: true,
+    });
+
     return () => (
       <div className="p-8 w-full h-full flex items-center justify-center">
         <div className="w-1/2 h-auto overflow-hidden shadow-lg flex items-start justify-start flex-col border dark:border-gray-800 rounded-lg">
-          <div className="w-full flex items-center justify-center border-b dark:border-gray-800">
+          <div className="w-full flex items-center justify-center border-b dark:border-gray-800 relative">
             <img
               alt="Forest"
               src="https://source.unsplash.com/1200x630/?forest"
               width="1200"
               height="630"
-              className="w-full h-auto"
+              className={\`w-full h-auto transition-opacity duration-300 \${state.loading ? 'opacity-0' : 'opacity-100'} \`}
+              onLoad={() => {
+                state.loading = false;
+              }}
             />
+            {state.loading && <div className="absolute w-full h-full top-0 left-0 animate-pulse bg-gray-100 dark:bg-gray-900" />}
           </div>
           <div className="p-4 w-full border-b dark:border-gray-800">
             <span className="text-xl">Lorem Ipsum</span>
@@ -73,7 +92,7 @@ const Card = defineComponent({
           <div className="p-4 w-full flex items-center justify-start flex-row-reverse">
             <button
               type="button"
-              className="text-purple-500 hover:text-purple-400"
+              className="bg-gray-800 py-2 px-4 text-white rounded-lg hover:bg-gray-700"
             >
               Action
             </button>
@@ -102,13 +121,13 @@ function Card() {
   return html\`
     <div class="p-8 w-full h-full flex items-center justify-center">
       <div class="w-1/2 h-auto overflow-hidden shadow-lg flex items-start justify-start flex-col border dark:border-gray-800 rounded-lg">
-        <div class="w-full flex items-center justify-center border-b dark:border-gray-800">
+        <div class="w-full flex items-center justify-center border-b dark:border-gray-800 relative">
           <img
             alt="Forest"
             src="https://source.unsplash.com/1200x630/?forest"
             width="1200"
             height="630"
-            class="w-full h-auto"
+            class="w-full h-auto transition-opacity duration-300 opacity-0"
           />
         </div>
         <div class="p-4 w-full border-b dark:border-gray-800">
@@ -120,7 +139,7 @@ function Card() {
         <div class="p-4 w-full flex items-center justify-start flex-row-reverse">
           <button
             type="button"
-            class="text-purple-500 hover:text-purple-400"
+            class="bg-gray-800 py-2 px-4 text-white rounded-lg hover:bg-gray-700"
           >
             Action
           </button>
@@ -130,8 +149,38 @@ function Card() {
   \`;
 }
 
+function Fallback() {
+  return html\`
+    <div class="absolute w-full h-full top-0 left-0 animate-pulse bg-gray-100 dark:bg-gray-900">
+    </div>
+  \`;
+}
+
+function renderFallback(root) {
+  root.querySelectorAll('img').forEach((el) => {
+    if (!el.complete) {
+      const fallback = document.createElement('div');
+  
+      render(Fallback(), fallback);
+  
+      el.parentNode.appendChild(fallback);
+  
+      el.addEventListener('load', () => {
+        el.classList.remove('opacity-0');
+        el.classList.add('opacity-100');
+  
+        el.parentNode.removeChild(fallback);
+      });
+    } else {
+      el.classList.remove('opacity-0');
+      el.classList.add('opacity-100');
+    }
+  });
+}
+
 export default function renderApp(root) {
   render(Card(), root);
+  renderFallback(root);
 }
 `;
 
