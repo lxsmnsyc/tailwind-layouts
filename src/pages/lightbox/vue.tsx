8cf4f4272baf7d78f90fa547de3c6676c8e56f0c
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-06-09 01:21:01 +0800

    Split `lightbox` source

---
+++ src/pages/lightbox/vue.tsx
@@ -0,0 +1,49 @@
+/** @jsx h */
+import {
+  h,
+  createApp,
+  defineComponent,
+  reactive,
+} from 'https://cdn.skypack.dev/vue@next/dist/vue.esm-browser.js';
+
+const Lightbox = defineComponent({
+  name: 'Lightbox',
+  setup () {
+    const state = reactive({
+      expand: false,
+      loading: true,
+    });
+    return () => (
+      <div className="p-8 w-full h-full flex items-center justify-center">
+        <div
+          className={`w-full cursor-pointer overflow-hidden flex items-center justify-center shadow-lg rounded-lg ${state.expand ? 'fixed top-0 left-0 w-screen h-screen p-16 bg-gray-900 bg-opacity-75 rounded-none border-none z-50' : 'relative'}`}
+          onClick={() => {
+            state.expand = !state.expand;
+          }}
+        >
+          <img
+            alt="Forest"
+            src="https://source.unsplash.com/1200x630/?forest"
+            width="1200"
+            height="630"
+            className={`w-full h-auto transition-opacity duration-200 ${state.loading ? 'opacity-0' : 'opacity-100'}`}
+            onLoad={() => {
+              state.loading = false;
+            }}
+          />
+          {state.loading && <div className="w-full absolute h-full animate-pulse bg-gray-100 dark:bg-gray-900" />}
+        </div>
+      </div>
+    );
+  }
+});
+
+export default function renderApp(root) {
+  const app = createApp(Lightbox);
+
+  app.mount(root);
+
+  return () => {
+    app.unmount();
+  };
+}
