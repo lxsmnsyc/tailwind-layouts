187f87333892e8ab2f62fa477ebe7083f45c75e8
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-06-09 00:40:39 +0800

    Split `app-window` source

---
+++ src/pages/app-window/lit-html.tsx
@@ -0,0 +1,29 @@
+import { html, render } from 'https://cdn.skypack.dev/lit-html';
+
+function AppWindow() {
+  return html`
+    <div class="p-8 w-full h-full flex items-center justify-center">
+      <div class="shadow-lg w-full flex items-start justify-start flex-col border dark:border-gray-800 rounded-lg">
+        <div class="w-full flex items-center justify-start relative p-1 border-b dark:border-gray-800">
+          <div class="p-1 flex items-center justify-center">
+            <div class="bg-red-500 m-1 w-3 h-3 rounded-full"></div>
+            <div class="bg-yellow-500 m-1 w-3 h-3 rounded-full"></div>
+            <div class="bg-green-500 m-1 w-3 h-3 rounded-full"></div>
+          </div>
+          <div class="w-full flex items-center justify-center absolute left-0">
+            <span class="font-sans text-xs text-gray-500 dark:text-gray-400">Hello World</span>
+          </div>
+        </div>
+        <div class="">
+          <div class="p-4">
+            <h1>Hello World</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+export default function renderApp(root) {
+  render(AppWindow(), root);
+}
---
+++ src/pages/app-window/react.tsx
@@ -0,0 +1,34 @@
+import React from 'https://cdn.skypack.dev/react';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+
+function AppWindow() {
+  return (
+    <div className="p-8 w-full h-full flex items-center justify-center">
+      <div className="shadow-lg w-full flex items-start justify-start flex-col border dark:border-gray-800 rounded-lg">
+        <div className="w-full flex items-center justify-start relative p-1 border-b dark:border-gray-800">
+          <div className="p-1 flex items-center justify-center">
+            <div className="bg-red-500 m-1 w-3 h-3 rounded-full" />
+            <div className="bg-yellow-500 m-1 w-3 h-3 rounded-full" />
+            <div className="bg-green-500 m-1 w-3 h-3 rounded-full" />
+          </div>
+          <div className="w-full flex items-center justify-center absolute left-0">
+            <span className="font-sans text-xs text-gray-500 dark:text-gray-400">Hello World</span>
+          </div>
+        </div>
+        <div className="">
+          <div className="p-4">
+            <h1>Hello World</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function renderApp(root) {
+  ReactDOM.render(<AppWindow />, root);
+
+  return () => {
+    ReactDOM.unmountComponentAtNode(root);
+  };
+}
