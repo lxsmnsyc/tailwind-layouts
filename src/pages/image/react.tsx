b2b4d5d73b1931549462e07ffcbe97c88019194c
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-06-09 01:23:37 +0800

    Split `image` source

---
+++ src/pages/image/react.tsx
@@ -0,0 +1,37 @@
+import React from 'https://cdn.skypack.dev/react';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+
+function Image() {
+  const [loading, setLoading] = React.useState(true);
+
+  return (
+    <div className="p-8 w-full h-full flex items-center justify-center">
+      <div className="w-full flex items-center justify-center flex-col">
+        <div className="w-3/4 overflow-hidden flex items-center justify-center shadow-lg rounded-lg relative">
+          <img
+            alt="Forest"
+            src="https://source.unsplash.com/1200x630/?forest"
+            width="1200"
+            height="630"
+            className={`w-full h-auto transition-opacity duration-200 ${loading ? 'opacity-0' : 'opacity-100'} `}
+            onLoad={() => {
+              setLoading(false);
+            }}
+          />
+          {loading && <div className="absolute w-full h-full top-0 left-0 animate-pulse bg-gray-100 dark:bg-gray-900" />}
+        </div>
+        <div className="w-full p-2 flex items-center justify-center">
+          <span className="text-xs">Source: Unsplash</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function renderApp(root) {
+  ReactDOM.render(<Image />, root);
+
+  return () => {
+    ReactDOM.unmountComponentAtNode(root);
+  };
+}
