23e3a792103835153183df21c328b311b7ef3a48
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-06-11 13:47:28 +0800

    Fix iframe permissions

--- src/components/Compiler.tsx
+++ src/components/Compiler.tsx
@@ -153,6 +153,8 @@ export default function Compiler(
       ref={setContainer}
       className="w-full h-full"
       srcDoc={IFRAME}
+      // https://github.com/sveltejs/svelte-repl/blob/master/src/Output/Viewer.svelte
+      sandbox="allow-popups-to-escape-sandbox allow-scripts allow-popups allow-forms allow-pointer-lock allow-top-navigation allow-modals allow-same-origin"
     />
   );
 }
