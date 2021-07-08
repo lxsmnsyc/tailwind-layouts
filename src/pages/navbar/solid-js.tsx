a84ad6a5a3424f895f280fb67dba87811a079cec
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-07-08 15:36:18 +0800

    Add `solid-js` for `navbar`

---
+++ src/pages/navbar/solid-js.tsx
@@ -0,0 +1,31 @@
+import { render } from 'https://cdn.skypack.dev/solid-js/web';
+import html from 'https://cdn.skypack.dev/solid-js/html';
+
+function Navbar() {
+  return html`
+    <div class="p-8 w-full h-full flex items-center justify-center">
+      <div class="w-full border dark:border-gray-800 flex items-center justify-between shadow-lg">
+        <div class="p-4">
+          <a href="#" class="text-xl font-bold">
+            Title
+          </a>
+        </div>
+        <div class="flex p-2 text-sm font-semibold">
+          <a href="#" class="p-2">
+            Page 1
+          </a>
+          <a href="#" class="p-2">
+            Page 2
+          </a>
+          <a href="#" class="p-2">
+            Page 4
+          </a>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+export default function renderApp(root: HTMLDivElement): () => void {
+  return render(Navbar, root);
+}
